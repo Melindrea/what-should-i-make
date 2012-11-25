1d6745f245cc4505bab7caf2a1322224d9814153
Marie Hogebrandt <webmistress@melindrea.net>
2012-11-25 19:47:19 +0100

    Updating Ideas to connect to User

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,8 @@
 class Idea < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :user_id
+
+  belongs_to :user
+
   def self.list(limit=5)
     limit(limit)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121125152427) do
+ActiveRecord::Schema.define(:version => 20121125182242) do
 
   create_table "ideas", :force => true do |t|
     t.string   "name"
     t.text     "description"
+    t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
