e69b8f952453a65f06bdb9719884447106a209e0
Marie Hogebrandt <webmistress@melindrea.net>
2012-11-26 06:33:24 +0100

    Setting a full title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #Returns the title on a per-page basis
+  def full_title(page_title, separator='|')
+    base_title = "What Should I Make?"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} #{separator} #{base_title}"
+    end
+  end
 end
+
